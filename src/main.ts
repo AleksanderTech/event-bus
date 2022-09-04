c0a4748a28f7c54111d59674e4057a58a363ec3c
AleksanderTech <aleksanderroztrop@gmail.com>
2022-09-04 11:31:17 +0200

    Typescript added, main created

---
+++ src/main.ts
@@ -0,0 +1,4 @@
+const input = document.querySelector('.input') as HTMLElement;
+const output = document.querySelector('.output') as HTMLElement;
+const addMessage = document.querySelector('.add-message') as HTMLElement;
+const messages = document.querySelector('.messages') as HTMLElement;
